ad8c429b79ca76231e06b9d3612be957f477ff29
Takeshi YAMAMURO <linguin.m.s@gmail.com>
2015-04-16 23:46:27 +0900

    Add new entries in scripts/define-all.sh

--- scripts/define-all.sh
+++ scripts/define-all.sh
@@ -8,6 +8,15 @@ val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
  * Regression functions
  */
 
+// sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS train_adadelta")
+sqlContext.sql("CREATE TEMPORARY FUNCTION train_adadelta AS 'hivemall.regression.AdaDeltaUDTF'")
+
+// sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS train_adagrad")
+sqlContext.sql("CREATE TEMPORARY FUNCTION train_adagrad AS 'hivemall.regression.AdaGradUDTF'")
+
+// sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS train_arow_regr")
+sqlContext.sql("CREATE TEMPORARY FUNCTION train_arow_regr AS 'hivemall.regression.AROWRegressionUDTF'")
+
 // sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS train_logregr")
 sqlContext.sql("CREATE TEMPORARY FUNCTION train_logregr AS 'hivemall.regression.LogressUDTF'")
 
@@ -17,6 +26,9 @@ sqlContext.sql("CREATE TEMPORARY FUNCTION train_logregr AS 'hivemall.regression.
 
 // sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS add_bias")
 sqlContext.sql("CREATE TEMPORARY FUNCTION add_bias AS 'hivemall.ftvec.AddBiasUDFWrapper'")
+
+// sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS sort_by_feature")
+sqlContext.sql("CREATE TEMPORARY FUNCTION sort_by_feature AS 'hivemall.ftvec.SortByFeatureUDF'")
 
 // sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS extract_feature")
 sqlContext.sql("CREATE TEMPORARY FUNCTION extract_feature AS 'hivemall.ftvec.amplify.ExtractFeatureUDFWrapper'")
@@ -32,4 +44,13 @@ sqlContext.sql("CREATE TEMPORARY FUNCTION amplify AS 'hivemall.ftvec.amplify.Amp
 
 // sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS rand_amplify")
 sqlContext.sql("CREATE TEMPORARY FUNCTION rand_amplify AS 'hivemall.ftvec.amplify.RandomAmplifierUDTF'")
+
+// sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS rescale")
+sqlContext.sql("CREATE TEMPORARY FUNCTION rescale AS 'hivemall.ftvec.scaling.RescaleUDF'")
+
+// sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS zscore")
+sqlContext.sql("CREATE TEMPORARY FUNCTION zscore AS 'hivemall.ftvec.scaling.ZScoreUDF'")
+
+// sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS normalize")
+sqlContext.sql("CREATE TEMPORARY FUNCTION normalize AS 'hivemall.ftvec.scaling.L2NormalizationUDF'")
 
