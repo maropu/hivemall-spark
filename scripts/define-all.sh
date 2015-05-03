8e3fc3e7bc807a9d9d4cc22608c1e0d79f51348b
Takeshi YAMAMURO <linguin.m.s@gmail.com>
2015-05-04 03:36:22 +0900

    Fix bugs in scripts/define-all.sh

--- scripts/define-all.sh
+++ scripts/define-all.sh
@@ -45,13 +45,13 @@ sqlContext.sql("CREATE TEMPORARY FUNCTION add_bias AS 'hivemall.ftvec.AddBiasUDF
 sqlContext.sql("CREATE TEMPORARY FUNCTION sort_by_feature AS 'hivemall.ftvec.SortByFeatureUDF'")
 
 // sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS extract_feature")
-sqlContext.sql("CREATE TEMPORARY FUNCTION extract_feature AS 'hivemall.ftvec.amplify.ExtractFeatureUDFWrapper'")
+sqlContext.sql("CREATE TEMPORARY FUNCTION extract_feature AS 'hivemall.ftvec.ExtractFeatureUDFWrapper'")
 
 // sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS extract_weight")
-sqlContext.sql("CREATE TEMPORARY FUNCTION extract_weight AS 'hivemall.ftvec.amplify.ExtractWeightUDFWrapper'")
+sqlContext.sql("CREATE TEMPORARY FUNCTION extract_weight AS 'hivemall.ftvec.ExtractWeightUDFWrapper'")
 
 // sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS add_feature_index")
-sqlContext.sql("CREATE TEMPORARY FUNCTION add_feature_index AS 'hivemall.ftvec.amplify.AddFeatureIndexUDFWrapper'")
+sqlContext.sql("CREATE TEMPORARY FUNCTION add_feature_index AS 'hivemall.ftvec.AddFeatureIndexUDFWrapper'")
 
 // sqlContext.sql("DROP TEMPORARY FUNCTION IF EXISTS amplify")
 sqlContext.sql("CREATE TEMPORARY FUNCTION amplify AS 'hivemall.ftvec.amplify.AmplifierUDTF'")
