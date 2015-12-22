1ec7e8532379f01063e59309ced84f6b2cbd170d
Takeshi YAMAMURO <linguin.m.s@gmail.com>
2015-12-23 00:18:43 +0900

    Add a test set for KDD2010a in ModelMixingSuite

--- project/HivemallSparkBuild.scala
+++ project/HivemallSparkBuild.scala
@@ -10,7 +10,8 @@ object HivemallSparkBuild extends Build {
       name := "hivemall-spark",
       organization := "maropu",
       version := "0.0.6",
-      scalaVersion := "2.10.4"
+      scalaVersion := "2.10.4",
+      javaOptions += "-Xmx8g"
       // add other settings here
     )
   )
--- src/test/scala/org/apache/spark/sql/hive/ModelMixingSuite.scala
+++ src/test/scala/org/apache/spark/sql/hive/ModelMixingSuite.scala
@@ -17,12 +17,13 @@
 
 package org.apache.spark.sql.hive
 
-import java.io.{BufferedReader, InputStream, InputStreamReader}
+import java.io.{BufferedInputStream, BufferedReader, InputStream, InputStreamReader}
 import java.net.URL
 import java.util.UUID
 import java.util.concurrent.{ExecutorService, Executors}
 
 import org.apache.commons.cli.Options
+import org.apache.commons.compress.compressors.CompressorStreamFactory
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.ml.feature.HmLabeledPoint
 import org.apache.spark.sql.functions.when
@@ -52,10 +53,40 @@ final class ModelMixingSuite extends SparkFunSuite with BeforeAndAfter {
     getDataFromURI(
       new URL("http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a9a").openStream,
       a9aLineParser)
+
   lazy val testA9aData: DataFrame =
     getDataFromURI(
       new URL("http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a9a.t").openStream,
       a9aLineParser)
+
+  // Load A9a training and test data
+  val kdd2010aLineParser = (line: String) => {
+    val elements = line.split(" ")
+    val (label, features) = (elements.head, elements.tail)
+    HmLabeledPoint(if (label == "1") 1.0f else 0.0f, features)
+  }
+
+  lazy val trainKdd2010aData: DataFrame =
+    getDataFromURI(
+      new CompressorStreamFactory().createCompressorInputStream(
+        new BufferedInputStream(
+          new URL("http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/kdda.bz2")
+            .openStream
+        )
+      ),
+      kdd2010aLineParser,
+      8)
+
+  lazy val testKdd2010aData: DataFrame =
+    getDataFromURI(
+      new CompressorStreamFactory().createCompressorInputStream(
+        new BufferedInputStream(
+          new URL("http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/kdda.t.bz2")
+            .openStream
+        )
+      ),
+      kdd2010aLineParser,
+      8)
 
   // Placeholder for a mix server
   var mixServExec: ExecutorService = _
@@ -183,7 +214,7 @@ final class ModelMixingSuite extends SparkFunSuite with BeforeAndAfter {
       // Build a model
       val model = {
         val groupId = s"${TestHive.sparkContext.applicationId}-${UUID.randomUUID}"
-        val res = invokeFunc(new HivemallOps(trainA9aData.part_amplify(1)), func,
+        val res = invokeFunc(new HivemallOps(trainKdd2010aData.part_amplify(1)), func,
           Seq[Column](add_bias($"features"), $"label",
             s"-mix localhost:${assignedPort} -mix_session ${groupId} -mix_threshold 2 -mix_cancel"))
         if (!res.columns.contains("conv")) {
@@ -194,7 +225,7 @@ final class ModelMixingSuite extends SparkFunSuite with BeforeAndAfter {
       }.as("feature", "weight")
 
       // Data preparation
-      val testDf = testA9aData
+      val testDf = testKdd2010aData
         .select(rowid(), $"label".as("target"), $"features")
         .cache
 
